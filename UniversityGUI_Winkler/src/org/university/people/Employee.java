1e0edd818b84830d96d94de5482ba3a33569f1e4
eewinkler <86085629+eewinkler@users.noreply.github.com>
2021-06-19 18:45:27 -0700

    Update Employee.java

--- UniversityGUI_Winkler/src/org/university/people/Employee.java
+++ UniversityGUI_Winkler/src/org/university/people/Employee.java
@@ -2,7 +2,7 @@ package org.university.people;
 
 public abstract class Employee extends Person {//employee is a child of person class
 	
-	public abstract double earns();//salary
-	public abstract void raise(double percent);//raise
+	public abstract double earns();//abstract salary method, used in professor and staff objects
+	public abstract void raise(double percent);//abstract raise method, used in professor and staff objects
 	
 }
