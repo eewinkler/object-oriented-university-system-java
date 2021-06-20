0d9d6ffc2cb30ea8089113db0374e56d6d18a7f7
eewinkler <86085629+eewinkler@users.noreply.github.com>
2021-06-19 18:27:27 -0700

    Update Department.java

--- UniversityGUI_Winkler/src/org/university/hardware/Department.java
+++ UniversityGUI_Winkler/src/org/university/hardware/Department.java
@@ -11,15 +11,15 @@ import java.io.Serializable;
 
 public class Department implements Serializable{
 
-	private String name;
-	private ArrayList<Course> courses;
-	private ArrayList<CampusCourse> coursesC;
-	private ArrayList<OnlineCourse> coursesO;
-	private ArrayList<Student> students;
-	private ArrayList<Professor> professors;
-	private ArrayList<Staff> staff;
+	private String name;//department name
+	private ArrayList<Course> courses;//department courses
+	private ArrayList<CampusCourse> coursesC;//department courses on campus in person
+	private ArrayList<OnlineCourse> coursesO;//department courses online
+	private ArrayList<Student> students;//students in department
+	private ArrayList<Professor> professors;//professors in department
+	private ArrayList<Staff> staff;//staff in department
 	
-	public Department() {
+	public Department() {//initializations
 		name = "unknown";
 		this.students = new ArrayList<Student>();
 		this.courses = new ArrayList<Course>();
@@ -29,112 +29,112 @@ public class Department implements Serializable{
 		this.coursesO = new ArrayList<OnlineCourse>();
 	}
 	
-	public String getDepartmentName() {
+	public String getDepartmentName() {//name getter
 		return name;
 	}
 	
-	public ArrayList<Course> getCourses() {
+	public ArrayList<Course> getCourses() {//course list getter
 		return courses;
 	}
 	
-	public void setDepartmentName(String newName) {
+	public void setDepartmentName(String newName) {//name setter
 		name = newName;
 	}
 	
-	public ArrayList<Course> getCourseList() {
+	public ArrayList<Course> getCourseList() {//course list getter
 		return courses;
 	}
 	
-	public ArrayList<CampusCourse> getCampusCourseList() {
+	public ArrayList<CampusCourse> getCampusCourseList() {//campus course list getter
 		return coursesC;
 	}
 	
-	public ArrayList<OnlineCourse> getOnlineCourseList() {
+	public ArrayList<OnlineCourse> getOnlineCourseList() {//online course list getter
 		return coursesO;
 	}
 	
-	public void addCourse(CampusCourse newCourse) {
-		courses.add(newCourse);
-		coursesC.add(newCourse);
-		newCourse.setDepartment(this);
+	public void addCourse(CampusCourse newCourse) {//add campus course to course list
+		courses.add(newCourse);//add to big course list
+		coursesC.add(newCourse);//add to campus course list
+		newCourse.setDepartment(this);//set department for course
 	}
 	
-	public void addCourse(OnlineCourse newCourse) {
-		courses.add(newCourse);
-		coursesO.add(newCourse);
-		newCourse.setDepartment(this);
+	public void addCourse(OnlineCourse newCourse) {//add online course to course list
+		courses.add(newCourse);//add to big course list
+		coursesO.add(newCourse);//add to online course list
+		newCourse.setDepartment(this);//set department for course
 	}
 	
-	public ArrayList<Student> getStudentList() {
+	public ArrayList<Student> getStudentList() {//student list getter
 		return students;
 	}
 	
-	public ArrayList<Professor> getProfessorList() {
+	public ArrayList<Professor> getProfessorList() {//professor list getter
 		return professors;
 	}
 	
-	public ArrayList<Staff> getStaffList() {
+	public ArrayList<Staff> getStaffList() {//staff list getter
 		return staff;
 	}
 	
-	public void addStudent(Student newStu) {
+	public void addStudent(Student newStu) {//add student
 		students.add(newStu);
 		newStu.setDepartment(this);
 	}
 	
-	public void addProfessor(Professor newPro) {
+	public void addProfessor(Professor newPro) {//add professor
 		professors.add(newPro);
 		newPro.setDepartment(this);
 	}
 	
-	public void addStaff(Staff newS) {
+	public void addStaff(Staff newS) {//add staff
 		staff.add(newS);
 		newS.setDepartment(this);
 	}
 	
-	public void get1Student() {
+	public void get1Student() {//gets one student from department
 		for (int i = 0; i < students.size(); i++) {
 			System.out.println(students.get(i).getName());
 		}
 	}
 	
-	public void get1Course() {
+	public void get1Course() {//gets one course from department
 		for (int i = 0; i < courses.size(); i++) {
 			System.out.println(courses.get(i).getDepartment().getDepartmentName() + courses.get(i).getCourseNumber() + " " + courses.get(i).getName());
 		}
 	}
 	
-	public void get1Professor() {
+	public void get1Professor() {//gets one professor from department
 		for (int i = 0; i < professors.size(); i++) {
 			System.out.println(professors.get(i).getName());
 		}
 	}
 	
-	public void get1Staff() {
+	public void get1Staff() {//gets one staff from department
 		for (int i = 0; i < staff.size(); i++) {
 			System.out.println(staff.get(i).getName());
 		}
 	}
 	
-	public void printStudentList() {
+	public void printStudentList() {//prints whole student name list
 		for (int i = 0; i < students.size(); i++) {
 			System.out.println(students.get(i).getName());
 		}
 	}
 	
-	public void printCourseList() {
+	public void printCourseList() {//prints whole course list with department, course number, and name
 		for (int i = 0; i < courses.size(); i++) {
 			System.out.println(courses.get(i).getDepartment().getDepartmentName() + courses.get(i).getCourseNumber() + " " + courses.get(i).getName());
 		}
 	}
 
-	public void printProfessorList() {
+	public void printProfessorList() {//prints whole professor name list
 		for (int i = 0; i < professors.size(); i++) {
 			System.out.println(professors.get(i).getName());
 		}
 	}
 	
-	public void printStaffList() {
+	public void printStaffList() {//prints whole staff name list
 		for (int i = 0; i < staff.size(); i++) {
 			System.out.println(staff.get(i).getName());
 		}
